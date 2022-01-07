545bd8c307c44a72c90d4df3bd2e24fa30606c5a
Tomas Baca <klaxalk@gmail.com>
2022-01-07 14:17:31 +0100

    update

--- scripts/build_mrs_uav_system_extended.sh
+++ scripts/build_mrs_uav_system_extended.sh
@@ -4,4 +4,4 @@
 REPO_PATH=`dirname "$0"`
 REPO_PATH=`( cd "$REPO_PATH/.." && pwd )`
 
-ludo singularity build -F $REPO_PATH/images/mrs_uav_system_extended.sif $REPO_PATH/recipes/mrs_uav_system_extended.def
+sudo singularity build -F $REPO_PATH/images/mrs_uav_system_extended.sif $REPO_PATH/recipes/mrs_uav_system_extended.def
--- scripts/execute.sh
+++ scripts/execute.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+if [ -z $1 ]; then
+  echo "argument required, please supply a command"
+  exit 1
+fi
+
+# get the path to this script
+REPO_PATH=`dirname "$0"`
+REPO_PATH=`( cd "$REPO_PATH/.." && pwd )`
+
 CMD="${@}"
 
-singularity exec --nv mrs.sif bash -c "source /opt/mrs/workspace/devel/setup.bash && export SHELL=/bin/bash && $CMD" 
+singularity exec --nv $REPO_PATH/images/mrs_uav_system_extended.sif bash -c "source /opt/mrs/workspace/devel/setup.bash && export SHELL=/bin/bash && $CMD"
