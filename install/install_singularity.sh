1c494eea53978af5cec4dfe747fe606c5cd106cc
Tomas Baca <klaxalk@gmail.com>
2022-06-10 13:22:41 +0200

    updated to singularity 3.10.0, updated install script

--- install/install_singularity.sh
+++ install/install_singularity.sh
@@ -1,23 +1,20 @@
-PNAME=$( ps -p "$$" -o comm= )
-SNAME=$( echo "$SHELL" | grep -Eo '[^/]+/?$' )
-if [ "$PNAME" != "$SNAME" ]; then
-  exec "$SHELL" -i "$0" "$@"
-  exit "$?"
-else
-  case $- in
-    *i*) ;;
-    *)
-      exec "$SHELL" -i "$0" "$@"
-      exit "$?"
-      ;;
-  esac
-fi
+#!/bin/bash
 
-RCFILE=~/."$SNAME"rc
+SHELL_NAME=$( echo "$SHELL" | grep -Eo '[^/]+/?$' )
+
+RCFILE=~/."$SHELL_NAME"rc
 
 # get the path to this script
 MY_PATH=`dirname "$0"`
 MY_PATH=`( cd "$MY_PATH" && pwd )`
+
+## | ------------------- configure versions ------------------- |
+
+export SINGULARITY_VERSION=3.10.0
+export GO_VERSION=1.18
+
+# export SINGULARITY_VERSION=3.9.5
+# export GO_VERSION=1.17
 
 ## | ----------------- install pre-requisities ---------------- |
 
@@ -36,36 +33,69 @@ sudo apt-get install -y \
 
 ## | ----------------------- install Go ----------------------- |
 
-cd /tmp
+INSTALL_GO=true
 
-export VERSION=1.17
-export OS=linux
-export ARCH=amd64  # Replace the values as needed
-wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz
-sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz
-rm go$VERSION.$OS-$ARCH.tar.gz
+# check if go is already installed
+if [ -e /usr/local/go/bin ]; then
 
-# add $PATH extension to the RC file
-num=`cat $RCFILE | grep -e "PATH.*go" | wc -l`
-if [ "$num" -lt "1" ]; then
+  echo pes
 
-  echo '
+  default=n
+  while true; do
+    if [[ "$unattended" == "1" ]]
+    then
+      resp=$default
+    else
+      read -t 10 -n 2 -p $'\e[1;32mGo is already installed, do you want to re-install it? Doing this might break your pre-existing setup... [y/n] (default: '"$default"$')\e[0m\n' resp || resp=$default
+    fi
+    response=`echo $resp | sed -r 's/(.*)$/\1=/'`
+
+    if [[ $response =~ ^(y|Y)=$ ]]
+    then
+      break
+    elif [[ $response =~ ^(n|N)=$ ]]
+    then
+      INSTALL_GO=false
+      break
+    else
+      echo " What? \"$resp\" is not a correct answer. Try y+Enter."
+    fi
+  done
+
+fi
+
+if $INSTALL_GO; then
+
+  cd /tmp
+
+  export OS=linux
+  export ARCH=amd64  # Replace the values as needed
+  wget https://dl.google.com/go/go$GO_VERSION.$OS-$ARCH.tar.gz
+  sudo tar -C /usr/local -xzvf go$GO_VERSION.$OS-$ARCH.tar.gz
+  rm go$GO_VERSION.$OS-$ARCH.tar.gz
+
+  # add $PATH extension to the RC file
+  num=`cat $RCFILE | grep -e "PATH.*go" | wc -l`
+  if [ "$num" -lt "1" ]; then
+
+    echo '
 # add Go to $PATH
 export PATH=/usr/local/go/bin:$PATH' >> $RCFILE
 
-  source $RCFILE
-fi
+    source $RCFILE
+  fi
 
-export PATH="/usr/local/go/bin:$PATH"
+  export PATH="/usr/local/go/bin:$PATH"
+
+fi
 
 ## | ------------------- install singularity ------------------ |
 
 cd /tmp
 
-export VERSION=3.9.5
-wget https://github.com/sylabs/singularity/releases/download/v${VERSION}/singularity-ce-${VERSION}.tar.gz
-tar -xzf singularity-ce-${VERSION}.tar.gz
-cd singularity-ce-${VERSION}
+wget https://github.com/sylabs/singularity/releases/download/v${SINGULARITY_VERSION}/singularity-ce-${SINGULARITY_VERSION}.tar.gz
+tar -xzf singularity-ce-${SINGULARITY_VERSION}.tar.gz
+cd singularity-ce-${SINGULARITY_VERSION}
 ./mconfig
 make -C builddir
 sudo make -C builddir install
