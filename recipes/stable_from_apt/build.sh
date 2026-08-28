c62eceb3b1749309918c58ac9e9b5b36a3e18f78
matthewvandyek <leonidasmith@protonmail.com>
2026-08-28 15:17:41 +0200

    build.sh apt: add support for tmpfs systems
    Linux systemd defaults to tmpfs, which means /tmp lives inside
    the RAM; that means /tmp has a small memory (capped at around
    half your RAM size). But build.sh unpacks Linux + ROS + Gazebo,
    causing overflow.
    
    This change activates only when tmpfs is being used and if so,
    forces the temporary build unpacked files to live in /var/tmp/apptainer.
    
    Signed-off-by: matthewvandyek <leonidasmith@protonmail.com>

--- recipes/stable_from_apt/build.sh
+++ recipes/stable_from_apt/build.sh
@@ -4,5 +4,25 @@
 THIS_PATH=`dirname "$0"`
 REPO_PATH=`( cd "$THIS_PATH/../.." && pwd )`
 THIS_PATH=`( cd "$THIS_PATH" && pwd )`
+BUILD_ENV=()
 
-sudo apptainer build $REPO_PATH/images/mrs_uav_system.sif $THIS_PATH/recipe.def
+is_tmpfs() {
+    if command -v findmnt >/dev/null 2>&1; then
+        [ "$(findmnt -n -o FSTYPE --target /tmp)" = "tmpfs" ]
+    else
+        [ "$(stat -f -c %T /tmp 2>/dev/null)" = "tmpfs" ]
+    fi
+}
+
+# systemd default caps /tmp at half your RAM size, this image unpacks
+# an entire linux filesystem => overflows => build fails
+# use another directory "tmp" directory instead
+
+if is_tmpfs; then
+    echo "[notice] will need sudo permissions to write in /var/tmp/apptainer/tmp"
+    DEFAULT_APPTAINER_TMPDIR=/var/tmp/apptainer/tmp
+    sudo mkdir -p /var/tmp/apptainer/tmp
+    BUILD_ENV=(APPTAINER_TMPDIR=$DEFAULT_APPTAINER_TMPDIR)
+fi
+
+sudo env "${BUILD_ENV[@]}" apptainer build $REPO_PATH/images/mrs_uav_system.sif $THIS_PATH/recipe.def
